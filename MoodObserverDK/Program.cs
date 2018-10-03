25f8faf039e1d6d70f6175ffed84c3fedb1591c3
Tue Thomas Ha Minh Son <thom875f@edu.eal.dk>
2018-10-03 08:38:21 +0200

    Commit changes in style and program

--- MoodObserverDK/Program.cs
+++ MoodObserverDK/Program.cs
@@ -20,6 +20,5 @@ namespace MoodObserverDK
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-    }
-    // Hej
+    } 
 }
